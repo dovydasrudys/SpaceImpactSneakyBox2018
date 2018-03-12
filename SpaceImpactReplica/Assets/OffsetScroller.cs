d412a691b5634363ff7e6155100348e8bc4484ec
RytisPundza <rytis.pundza@gmail.com>
2018-03-12 19:09:04 +0200

    SP12

---
+++ SpaceImpactReplica/Assets/OffsetScroller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffsetScroller : MonoBehaviour
+{
+
+    float scrollSpeed = -5f;
+    Vector2 savedOffset;
+
+    void Start()
+    {
+        savedOffset = transform.position;
+    }
+
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 19.94f);
+        transform.position = savedOffset + Vector2.right * newPos;
+    }
+
+    
+}
