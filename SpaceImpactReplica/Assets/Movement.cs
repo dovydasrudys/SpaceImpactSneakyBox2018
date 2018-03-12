05ff4da3c5ccfae4698cb666f69da02e5842751a
Eimontas Dambrauskas <eimontasdambrauskas@gmail.com>
2018-03-12 17:41:43 +0200

    SP11

---
+++ SpaceImpactReplica/Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed;
+
+    private Vector2 direction = Vector2.zero;
+    Vector3 movement = new Vector3();
+    float h;
+    float v;
+
+    void FixedUpdate()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        movement.Set(h, v, 0);
+
+        transform.localPosition += movement * speed;
+        Debug.Log(h + " " + v);
+    }
+
+}
